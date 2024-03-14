3968a9244dd5086076d1bd7e0cebd389b79a3674
Mustafa <mastafa3alie@gmail.com>
2024-03-15 01:25:14 +0200

    user table

--- database/migrations/2023_10_17_000002_create_users_table.php
+++ database/migrations/2023_10_17_000002_create_users_table.php
@@ -13,20 +13,20 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        if (!Schema::hasTable('users'))
+        if (!Schema::hasTable('users')) {
             Schema::create('users', function (Blueprint $table) {
                 $table->id();
-                $table->string('full_name');
-                $table->string('full_name_latin')->nullable();
+                $table->string('name');
                 $table->string('email')->unique();
                 $table->string('phone')->unique();
-                $table->unsignedBigInteger('country_id')->nullable();
                 $table->text('profile_photo_path')->nullable();
                 $table->text('two_factor_secret')->nullable();
                 $table->text('two_factor_recovery_codes')->nullable();
                 $table->timestamp('two_factor_confirmed_at')->nullable();
                 $table->timestamp('email_verified_at')->nullable();
-                $table->string('password');
+                $table->enum('type', ['admin','vendor','customer']);
+                $table->unsignedBigInteger('model_id')->nullable();
+                $table->string('password')->nullable();
                 $table->integer('active')->default(1);
                 $table->rememberToken();
                 $table->unsignedBigInteger('created_by')->nullable();
@@ -37,6 +37,7 @@ class CreateUsersTable extends Migration
                 $table->foreign('created_by')->references('id')->on('users');
                 $table->foreign('updated_by')->references('id')->on('users');
             });
+        }
     }
 
     /**
