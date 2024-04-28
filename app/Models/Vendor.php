9477e226b789cb15648230c398d1097f19b4cc90
ramadan <ramadanabdo08@github.com>
2024-04-29 01:53:07 +0300

    add dashboard

--- app/Models/Vendor.php
+++ app/Models/Vendor.php
@@ -36,7 +36,7 @@ class Vendor extends Model
     }
     public function userVendor(): ?HasOne
     {
-        return $this->hasOne(User::class,'model_id')->where('type','=',"vendor");
+        return $this->hasOne(User::class,'model_id');
     }
 //    public function user()
 //    {
