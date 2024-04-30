496b239da820e23b071337257f5f57b7b9f15854
ramadan <ramadanabdo08@github.com>
2024-04-30 16:47:24 +0300

    add service category

--- app/Http/Requests/General/ServiceCategoryRequest.php
+++ app/Http/Requests/General/ServiceCategoryRequest.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Requests\General;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\ValidationException;
 
 class ServiceCategoryRequest extends FormRequest
 {
@@ -64,5 +67,10 @@ class ServiceCategoryRequest extends FormRequest
         return [
 
         ];
+    }
+    protected function failedValidation(Validator $validator)
+    {
+        $errors = (new ValidationException($validator))->errors();
+        throw new HttpResponseException(apiResponse(false, null, 'Validation Error', $errors, 401));
     }
 }
