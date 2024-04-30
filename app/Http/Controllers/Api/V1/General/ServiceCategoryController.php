df19db31e6a64717989de4cfd6bb0c92d5d43709
ramadan <ramadanabdo08@github.com>
2024-04-30 16:45:59 +0300

    add service category

--- app/Http/Controllers/Api/V1/General/ServiceCategoryController.php
+++ app/Http/Controllers/Api/V1/General/ServiceCategoryController.php
@@ -7,7 +7,6 @@ use App\Http\Requests\General\ServiceCategoryRequest;
 use App\Http\Requests\PaginateRequest;
 use App\Models\ServiceCategory;
 use App\Services\General\ServiceCategoryService;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Schema;
 use function response;
 
@@ -39,7 +38,7 @@ class ServiceCategoryController extends Controller
         return response()->apiSuccess($this->service->get($id));
     }
 
-    public function store(Request $request)
+    public function store(ServiceCategoryRequest $request)
     {
         $data = $request->all();
         dd($data);
