35cbc9566b8ad9df26b701b8f510cce8edfcc39b
ramadan <ramadanabdo08@github.com>
2024-04-30 17:39:30 +0300

    add service category

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Subscription extends Model
 {
     use HasFactory,SoftDeletes;
+    protected $guarded=[];
     public function vendor(): ?BelongsTo
     {
         return $this->belongsTo(Vendor::class,'vendor_id');
