e698acce8d284b9a4c1b618481bb269a600ca153
ramadanaboali <ramadanabdo08@gmail.com>
2024-05-22 19:13:39 +0300

    start to add vendor reorts

--- app/Console/Commands/installPermissions.php
+++ app/Console/Commands/installPermissions.php
@@ -144,6 +144,12 @@ class installPermissions extends Command
         if ($vendorRole) {
             $vendorPermissions = Permission::whereIn('model_type', ['vendor','general'])->get();
             $vendorRole->syncPermissions($vendorPermissions);
+            $users = User::where('type', 'vendor')->get();
+            foreach ($users as $user) {
+                $user->syncRoles($vendorRole->id);
+            }
+            Artisan::call('cache:clear');
+            $this->info("All Vendor Permissions assigned to user vendors");
         }
     }
 }
